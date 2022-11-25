3e1aecf7cfb194b2ec1fcb5c60b36171f71b2d0f
Ruben Hernandez Alvarado <rubhernandez42@gmail.com>
2022-11-24 23:54:38 -0600

    adding js and json files

---
+++ src/scope/function.js
@@ -0,0 +1,7 @@
+function greeting() {
+    let username = 'Ruben';
+    console.log('Ruben')
+    if (username === 'Ruben') {
+        console.log(`Hello ${username}!`)
+    }
+}
---
+++ src/scope/global.js
@@ -0,0 +1,25 @@
+// variable 
+
+var a; // declarando 
+var b = 'b'; // declarando y asignando 
+b = 'bb'; // re-asignando 
+var a = 'aa'; // re-declarando 
+
+// GLOBAL SCOPE 
+
+var fruit = 'Apple'; // GLOBAL  
+
+function bestFruit() {
+    console.log(fruit);
+}
+
+bestFruit();
+
+function countries() {
+    country = 'Mexico'; // GLOBAL 
+    console.log(country);
+}
+
+
+countries();
+console.log(country);
